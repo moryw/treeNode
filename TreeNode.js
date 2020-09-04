11daba1020aa49a3b6dd75981fcdf5497ca29efa
Mory W <matt@skitskat.xyz>
2020-09-04 10:58:01 -0500

    Add addChild mehtod to TreeNode class

--- TreeNode.js
+++ TreeNode.js
@@ -3,9 +3,25 @@ class TreeNode {
     this.data = data
     this.children = []
   }
+
+  addChild(child) {
+    if (child instanceof TreeNode) {
+      this.children.push(child)
+    } else {
+      let newChild = new TreeNode(child)
+      this.children.push(newChild)
+    }
+  }
 }
 
 const tree = new TreeNode(1)
+console.log(tree)
+
+tree.addChild(15)
+console.log(tree)
+
+const newTree = new TreeNode(30)
+tree.addChild(newTree)
 console.log(tree)
 
 module.exports = TreeNode
